665d745f347d1876eae700aa5385cb3e995f6d34
teseimela-bit <teseimela@gmail.com>
2026-02-02 11:42:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ elif menu == "STEM Data Explorer":
         ["Solar Cell Performance", "Charge Transport", "XRD Data"]
     )
     st.image(
-        "C:/Users/tesei/Downloads/streamlit_files/streamlit_files/Solar_cell_schem.jpg",
+        "Solar_cell_schem.jpg",
         caption = "Schematic diagram of organic solar cell containg copper nanorods and it's energy band diagram"
         )
     if data_option == "Solar Cell Performance":
